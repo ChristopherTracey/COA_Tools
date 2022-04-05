5219f8ed2606e7314eb74d2577683797d511267a
Christoper Tracey <ctracey@paconserve.org>
2022-04-05 16:18:02 -0400

    some index cleanup

--- scripts/12_Indexes.r
+++ scripts/12_Indexes.r
@@ -17,14 +17,6 @@ require(here)
 
 source(here::here("scripts", "00_PathsAndSettings.r"))
 
-# olddatabasename <- "coa_bridgetest_previous.sqlite" 
-# olddatabasename <- here::here("_data","output",olddatabasename)
-# 
-# 
-# db <- dbConnect(SQLite(), dbname=olddatabasename) # connect to the database
-# sgcnXpu <- dbReadTable(db, "lu_sgcnXpu_all") # write the table to the sqlite
-# dbDisconnect(db) # disconnect the db
-
 db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
   dbExecute(db, "CREATE INDEX habitat ON lu_HabTerr (unique_id, Code);")
   dbExecute(db, "CREATE INDEX habitataq ON lu_LoticData (unique_id, SUM_23);")
@@ -33,16 +25,15 @@ db <- dbConnect(SQLite(), dbname=databasename) # connect to the database
   dbExecute(db, "CREATE INDEX natbound ON lu_NaturalBoundaries (unique_id);")
   dbExecute(db, "CREATE INDEX proland ON lu_ProtectedLands_25 (unique_id);")
   dbExecute(db, "CREATE INDEX threats ON lu_threats (unique_id);")
-
 dbDisconnect(db)
 
-CREATE INDEX habitat ON lu_HabTerr (unique_id, Code);
-CREATE INDEX habitataq ON lu_LoticData (unique_id, SUM_23);
-CREATE INDEX maindex ON lu_sgcnXpu_all (unique_id,ELSeason);
-CREATE INDEX muni ON lu_muni (unique_id);
-CREATE INDEX natbound ON lu_NaturalBoundaries (unique_id);
-CREATE INDEX proland ON lu_ProtectedLands_25 (unique_id);
-CREATE INDEX threats ON lu_threats (unique_id);
+# CREATE INDEX habitat ON lu_HabTerr (unique_id, Code);
+# CREATE INDEX habitataq ON lu_LoticData (unique_id, SUM_23);
+# CREATE INDEX maindex ON lu_sgcnXpu_all (unique_id,ELSeason);
+# CREATE INDEX muni ON lu_muni (unique_id);
+# CREATE INDEX natbound ON lu_NaturalBoundaries (unique_id);
+# CREATE INDEX proland ON lu_ProtectedLands_25 (unique_id);
+# CREATE INDEX threats ON lu_threats (unique_id);
 
 
 
